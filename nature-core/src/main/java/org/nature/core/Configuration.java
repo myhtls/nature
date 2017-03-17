8bf451770ca4f4d2234af8e8c68f1d8625cc6dcf
胡天龙 <myhtls@163.com>
2017-03-18 07:16:02 +0800

    忘提了

---
+++ nature-core/src/main/java/org/nature/core/Configuration.java
@@ -0,0 +1,13 @@
+package org.nature.core;
+
+public class Configuration {
+	
+	
+	public static final String ENTITYMANAGERAPP = "appPu";
+	
+	public static final String ENTITYMANAGERPICKETLINK = "picketlink";
+	
+	
+	
+
+}
